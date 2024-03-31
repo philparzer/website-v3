b2676822aa140d4f697d727d85bc7739a621037f
philparzer <parzerphilipp@gmail.com>
2024-04-01 00:47:06 +0200

    remove test message

--- components/rive-animation.tsx
+++ components/rive-animation.tsx
@@ -37,7 +37,6 @@ const RiveAnimation = ({ src, stateMachine, isActive }: RiveAnimationProps) => {
       <div className="w-full absolute z-10 -top-[400px]  left-0 h-20 bg-gradient-to-b from-whiteout to-transparent" />
       <div className="h-full absolute z-10 -top-[200px] w-20 right-[-5px]  bg-gradient-to-l blur-[1px] from-whiteout via-whiteout to-transparent" />
       <div className="w-20  absolute z-10 -top-[200px] left-0 h-full bg-gradient-to-r blur-[1px] from-whiteout via-whiteout to-transparent" />
-      <div className="z-20 absolute top-0 left-0"> {isActive && "active"}</div>
       <RiveComponent className="invert-filter pointer-events-none absolute -top-[400px] left-0 w-full h-full " />
     </div>
   );
